8f7d0bcb8b9cd692efd10ef50b9489bbe37c28f5
kit jonathan ho <j.hokit80@gmail.com>
2016-10-08 22:39:21 +0800

    passport

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 var express = require('express');
 var mongoose = require('mongoose');
+// var MongoStore = require("connect-mongo")(session);
+var passport = require("passport");
 var bodyParser = require("body-parser");
 var ejs = require("ejs");
 var engine = require("ejs-mate");
-var cookieParser = require("cookieParser");
+var cookieParser = require("cookie-parser");
 var morgan = require("morgan");
-var session = require("session");
+var session = require("express-session");
 var app = express();
 
-// Note: A cookie is send to the server, and the server will store the cookie in a session. Hence cookie is stored on the browser while session is stored on the server and it is usually associated with a given user. Session will be stored in MongoDB.
+// Note: A cookie is send to the server, and the server will store the cookie in a session. Hence cookie is stored on the browser while session is stored on the server and it is usually associated with a given user. Session will be stored in MongoDB. Connect-mongo is a library used to store session data into mongoDB. Before the cookie is created, we need to authenticate user 1st, hence, will need to include passport
 
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cookieParser());
+// app.use(session({
+//   resave: true,
+//   saveUninitialized: true,
+//   secret: "hello",
+//   store: new MongoStore({url: , autoReconnect: true})
+// }))
 app.engine("ejs", engine);
 app.set("view engine", "ejs");
 
