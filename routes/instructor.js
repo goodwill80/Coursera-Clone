8996ec3dca39184a17d752133849188c4eae220c
kit jonathan ho <j.hokit80@gmail.com>
2016-10-09 23:46:16 +0800

    added create course route and html

--- routes/instructor.js
+++ routes/instructor.js
@@ -26,14 +26,14 @@ module.exports = function(app) {
             foundUser.coursesTeach.push({ course: course._id});
             foundUser.save(function(err){
               if (err) return next(err);
-              res.redirect('teacher/teacher-dashboard');
+              res.redirect('teacher/dashboard');
             });
           });
         }
       ]);
     });
 
-    app.get('/teacher/teacher-dashboard', function(req, res, next) {
+    app.get('/teacher/dashboard', function(req, res, next) {
       User.findOne({_id: req.user._id})
       .populate("coursesTeach.course")
       .exec(function(err, foundUser){
@@ -41,7 +41,36 @@ module.exports = function(app) {
       });
     })
 
+    app.route("/create-course")
 
+      .get(function(req, res, next) {
+        res.render("teacher/create-course")
+      })
+
+      .post(function(req, res, next) {
+        async.waterfall([
+          function(callback) {
+            var course = new Course();
+            course.title = req.body.title;
+            course.price = req.body.price;
+            course.desc = req.body.desc;
+            course.wistiaId = req.body.wistiaId;
+            course.ownByTeacher = req.user._id;
+            course.save(function(err){
+              callback(err, course);
+            });
+          },
+          function(course, callback) {
+            User.findOne({_id: req.user._id}, function(err, foundUser){
+              foundUser.coursesTeach.push({ course: course._id});
+              foundUser.save(function(err){
+                if (err) return next(err);
+                res.redirect('teacher/dashboard');
+              });
+            });
+          }
+        ]);
+      });
 
 
 
