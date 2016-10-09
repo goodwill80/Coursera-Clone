0034c375b1406ddb2f66da9a409e18018ee07959
kit jonathan ho <j.hokit80@gmail.com>
2016-10-09 12:54:13 +0800

    routes

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,21 @@ var passportConfig = require('../config/passport');
 
 module.exports = function(app) {
 
-  app.get('/auth/facebook', passport.authenticate('facebook'))
+  app.get('/auth/facebook', passport.authenticate('facebook', {scope: 'email'}));
+
+  app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+    successRedirect: '/profile',
+    failureRedirect: '/login'
+  }));
+
+  app.get('/logout', function(req, res, next) {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.get('/profile', function(req, res, next){
+    res.render('accounts/profile');
+  })
 
 
 
