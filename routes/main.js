1e2644651b8a4518004d714b9006f7e6c27ee114
kit jonathan ho <j.hokit80@gmail.com>
2016-10-10 19:09:21 +0800

    added routes and logic for user and teacher courses

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,6 @@
 var User = require("../models/user");
 var Course = require("../models/course");
+var async = require("async");
 
 module.exports = function(app) {
 
@@ -16,6 +17,49 @@ module.exports = function(app) {
       res.render('courses/courses', { courses: courses});
     })
   });
+
+  app.get("/courses/:id", function(req, res, next) {
+    async.parallel([
+      //1st function
+      function(callback) {
+        Course.findOne({ _id: req.params.id }, function(err, foundCourse){
+          callback(err, foundCourse);
+        });
+      },
+      //2nd function
+      function(callback) {
+        User.findOne({ _id: req.user._id, 'coursesTaken.course': req.params.id })
+        .populate('coursesTaken.course')
+        .exec(function(err, foundUserCourse) {
+          callback(err, foundUserCourse);
+        })
+      },
+      //3rd function
+      function(callback) {
+        User.findOne({ _id: req.user._id, 'coursesTeach.course': req.params.id })
+        .populate('coursesTeach.course')
+        .exec(function(err, foundUserCourse) {
+          callback(err, foundUserCourse);
+        })
+      }
+      //callback on all 3 functions above
+    ], function(err, results) {
+        //1st function
+        var course = results[0];
+        //2nd function
+        var userCourse = results[1];
+        //3rd function
+        var teacherCourse = results[2];
+        //combining all 3 functions in a condition
+        if (userCourse === null && teacherCourse === null) {
+          res.render('courses/courseDesc', { course: course} );
+        } else if (userCourse === null && teacherCourse != null) {
+          res.render('courses/course', { course: course } );
+        } else {
+          res.render('courses/course', { course: course } );
+        }
+    })
+  })
 
 
 }
